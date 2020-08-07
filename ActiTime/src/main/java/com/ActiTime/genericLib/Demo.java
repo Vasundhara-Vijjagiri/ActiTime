ad0f6e854ed6828f923a28b7708deae585b47e2e
vassu <vassu.vijjagiri@gmail.com>
2020-08-07 10:38:54 +0530

    SSECOND COMMIT

---
+++ ActiTime/src/main/java/com/ActiTime/genericLib/Demo.java
@@ -0,0 +1,14 @@
+package com.ActiTime.genericLib;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void run()
+	{
+		Reporter.log("am run",true);
+	}
+	
+
+}
