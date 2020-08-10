7cdde3f627204423b2275c4d4e1b7f79f19e2919
vassu <vassu.vijjagiri@gmail.com>
2020-08-10 17:08:10 +0530

    My second commit

---
+++ src/main/java/com/ActiTime/genericLib/App.java
@@ -0,0 +1,14 @@
+package com.ActiTime.genericLib;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class App {
+	@Test
+	public void run()
+	{
+		Reporter.log("Hi Run",true);
+		Reporter.log("Hi Run",true);
+	}
+
+}
